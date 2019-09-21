5fa794fdbefc0227fc4ea4b323726fee8003944b
thszk <thsuzuqui@gmail.com>
2019-09-21 01:17:05 -0400

    updating project

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 const fs = require('fs')
 const path = require('path')
 
-// read source code
-file = fs.readFileSync(path.resolve('./assets/input.txt'), 'utf-8')
-// read language notation
-language = fs.readFileSync(path.resolve('./assets/language.txt'), 'utf-8')
+const language = require('./language')
 
-// clean the code
-const codeCleaner = code => code.split(/\s/)
+// method to read source code file
+const readCode = () => fs.readFileSync(path.resolve('./assets/input.txt'), 'utf-8')
 
-// remove comments
-const removeComments = code => {
-  let filtered = code.filter(element => !element.startsWith('{')) // open comment
-  filtered = filtered.filter(element => !element.endsWith('}')) // close comment
-  return filtered
+// method to remove comments | add spaces before and after a alphanum character | creates an array from cleaned code
+const codeCleaner = (code) => {
+  return code
+            .replace(/{.*}/g, ' ')
+            .replace(/([^a-zA-Z0-9_\s])/g, ' $1 ')
+            .split(/\s+/)
 }
 
-const find = (code, lang) => {
-  file.forEach(element => {
-    
+// find in language grammar
+const findInGrammar = (target, index) => {
+  let match = language.keywords.find(element => element.name === target)
+  
+  if (!match)
+    match = language.symbols.find(element => element.name === target)
+  
+  if (!match)
+    match = language.others.find(element => target.match(element.name)) // tem que validar se o target é uma das regex contidas em language.others
+  
+  // console.log(match);
+  return match ? { name: target, value: match.value, index } : undefined
+}
+
+// tokenization
+const find = (code) => {
+  let tokens = [], newToken
+  code.forEach((element, index) => {
+    newToken = findInGrammar(element, index)
+    if (newToken) tokens.push(newToken)
   })
+  console.table(tokens)
 }
 
 const main = () => {
+  file = readCode()
   file = codeCleaner(file)
-  file = removeComments(file)
-  console.log(file)
+  find(file)
 }
 
 main()
