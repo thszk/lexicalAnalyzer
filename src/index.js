21ab895a574a594549458252ffb8e37091666b39
thszk <thsuzuqui@gmail.com>
2019-09-27 14:15:18 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ const readCode = () => fs.readFileSync(path.resolve('./assets/input.txt'), 'utf-
 // method to remove comments | add spaces before and after a alphanum character | creates an array from cleaned code
 const codeCleaner = (code) => {
   return code
-            .replace(/{.*}/g, ' ') // comments
-            .replace(/([\(\)])/g, ' $1 ') // parentheses
-            .replace(/([^a-zA-Z0-9_\s][^a-zA-Z0-9_\s]?)/g, ' $1 ') // symbols
+            .replace(/{(.|\n)*?}|{(.|\n)*/g, ' ') // comments
+            .replace(/(:=|<=|>=|<>|[^\w\s])/g, ' $1 ') // symbols
             .split(/\s+/) // spaces
 }
 
@@ -37,8 +36,6 @@ const find = (code) => {
   return tokens
 }
 
-const lexicalAnalyzer = () => {
-  return find(codeCleaner(readCode()))
-}
+const lexicalAnalyzer = () => find(codeCleaner(readCode()))
 
 console.table(lexicalAnalyzer())
